bfc7866e2f5f9ebc1f6f642f3f12e2a95314f8ef
tmax818 <tylermaxwell661@gmail.com>
2022-08-01 16:36:29 -0700

    update afternoon

--- lecture/01day/demos/composite.py
+++ lecture/01day/demos/composite.py
@@ -1,10 +1,14 @@
-person = "Cameron"
 
-person_list = ['Cameron', 25, True, True]
-person = {'name': 'Cameron', 'age': 25}
+
 
 people = [
     {'name': 'Tyler', 'age': 47},
     {'name': 'Duque', 'age': 26},
     {'name': 'Lucie', 'age': 25}
 ]
+
+# print(people[0][1])
+
+for person in people:
+    print(person['name'])
+    print('hi mom')
