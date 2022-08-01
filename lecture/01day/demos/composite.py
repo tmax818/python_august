ca32337e75ebccd7fc44a035670cb643c45ab5f7
tmax818 <tylermaxwell661@gmail.com>
2022-08-01 14:58:32 -0700

    update

--- lecture/01day/demos/composite.py
+++ lecture/01day/demos/composite.py
@@ -6,5 +6,5 @@ person = {'name': 'Cameron', 'age': 25}
 people = [
     {'name': 'Tyler', 'age': 47},
     {'name': 'Duque', 'age': 26},
-    {'name': 'Lucie', 'age': 22}
+    {'name': 'Lucie', 'age': 25}
 ]
