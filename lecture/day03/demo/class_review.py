e1f6b07ad920fd278f6351cea79c11f79b33fd57
tmax818 <tylermaxwell661@gmail.com>
2022-08-03 17:41:02 -0700

    update end

--- lecture/day03/demo/class_review.py
+++ lecture/day03/demo/class_review.py
@@ -1,14 +1,9 @@
-
-
 users_list = [
     {'first': 'Tre', 'last': 'Mays', 'email': 'tdog@hotmail.com'},
     {'first': 'Lucie', 'last': 'Chevreuil', 'email': 'lc@hotmail.com'},
     {'first': 'Nisrine', 'last': 'Kane', 'email': 'nk@gmail.com'},
-
-
+    {'first': 'D', 'last': 34, 'email': 'hi mom'}
 ]
-
-
 
 class User:
 
@@ -27,9 +22,26 @@ class User:
     @classmethod
     def register(cls, the_list):
         for user_in_list in the_list:
-            cls.users.append(User(user_in_list))
+            if not User.validate_user(user_in_list):
+                print('sorry, invalid')
+            else:
+                cls.users.append(User(user_in_list))
+    
+    @staticmethod
+    def validate_user(user:dict)-> bool:
+        is_valid = True
+        if len(user['first']) < 2:
+            
+            is_valid = False
+        if len(user['last']) < 2:
+            is_valid = False
+        return is_valid
 
-user1 = User(users_list)
+
+if not User.validate_user(users_list[-1]):
+    print("Sorry, D! you need a longer name")    
+
+user1 = User(users_list[-1])
 user2 = User(users_list[1])
 
 user_instances = User.register(users_list)
