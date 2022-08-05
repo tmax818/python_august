6f94689d7cb5ab75e7a2cc7637d36d43f2aae2a0
tmax818 <tylermaxwell661@gmail.com>
2022-08-05 12:16:58 -0700

    update

--- lecture/day05/demo/flask_form/server.py
+++ lecture/day05/demo/flask_form/server.py
@@ -1,14 +1,15 @@
-from flask import Flask, render_template, request, redirect, session 
-app = Flask(__name__)   
-app.secret_key = "any string I want..."
+from flask import Flask, render_template, request, redirect, session
 
-@app.route('/')        
-def hello_world():
+app = Flask(__name__)
+app.secret_key = "Any string you want"
+
+@app.route('/')
+def index():
     return render_template('index.html')
 
-@app.route('/form', methods=['POST'])
-def handle_form_data():
-    print(request.form)
+@app.route('/form', methods=['post'])
+def handle_form():
+    print(request.form['key'])
     session['data'] = request.form
     return redirect('/')
 
@@ -16,6 +17,15 @@ def handle_form_data():
 def clear():
     session.clear()
     return redirect('/')
+
+@app.route('/up')
+def up():
+    if 'count' in session:
+        session['count'] += 1
+    else:
+        session['count'] = 1
+    return redirect('/')
+
 
 if __name__=="__main__":     
     app.run(debug=True)   
