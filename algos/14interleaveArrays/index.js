f248998ec7b47743578d52c69252020fd0f84a03
tmax818 <tylermaxwell661@gmail.com>
2022-08-26 08:38:56 -0700

    final update

---
+++ algos/14interleaveArrays/index.js
@@ -0,0 +1,32 @@
+/* 
+  Given two arrays, interleave them into one new array.
+  The arrays may be different lengths. The extra items should be added to the
+  back of the new array.
+*/
+
+const arrA1 = [1, 2, 3];
+const arrB1 = ["a", "b", "c"];
+const expected1 = [1, "a", 2, "b", 3, "c"];
+
+const arrA2 = [1, 3];
+const arrB2 = [2, 4, 6, 8];
+const expected2 = [1, 2, 3, 4, 6, 8];
+
+const arrA3 = [1, 3, 5, 7];
+const arrB3 = [2, 4];
+const expected3 = [1, 2, 3, 4, 5, 7];
+
+const arrA4 = [];
+const arrB4 = [42, 0, 6];
+const expected4 = [42, 0, 6];
+
+/**
+ * Interleaves two arrays into a new array. Interleaving means alternating
+ * the items starting from the first array.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Array<any>} arr1
+ * @param {Array<any>} arr2
+ * @returns {Array<any>} A new array of interleaved items.
+ */
+function interleaveArrays(arr1, arr2) {}
